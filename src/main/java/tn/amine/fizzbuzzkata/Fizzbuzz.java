5d66c5e041283667879e875f30428f07b4b59ea1
aminejrad1 <mohamedamine.jrad@esprit.tn>
2022-07-23 20:32:25 +0100

    test 10 iteration

--- src/main/java/tn/amine/fizzbuzzkata/Fizzbuzz.java
+++ src/main/java/tn/amine/fizzbuzzkata/Fizzbuzz.java
@@ -2,16 +2,19 @@ package tn.amine.fizzbuzzkata;
 
 public class Fizzbuzz {
 
-    public static String play(int number) {
-        if(number % 15 == 0)
+    public static String play(int number1, int number2) {
+        if(number1!=number2)
+            return number1 + String.valueOf(number2);
+
+        if(number1 % 15 == 0)
             return "FizzBuzz";
 
-        if(number % 3 == 0)
+        if(number1 % 3 == 0)
             return "Fizz";
 
-        if(number % 5 == 0)
+        if(number1 % 5 == 0)
             return "Buzz";
 
-        return String.valueOf(number);
+        return String.valueOf(number1);
     }
 }
--- src/test/java/tn/amine/fizzbuzzkata/FizzbuzzKataApplicationTests.java
+++ src/test/java/tn/amine/fizzbuzzkata/FizzbuzzKataApplicationTests.java
@@ -10,47 +10,52 @@ class FizzbuzzKataApplicationTests {
 
     @Test
     void testWithNumberIs1() {
-        assertEquals("1", Fizzbuzz.play(1));
+        assertEquals("1", Fizzbuzz.play(1, 1));
     }
 
     @Test
     void testWithNumberIs2() {
-        assertEquals("2", Fizzbuzz.play(2));
+        assertEquals("2", Fizzbuzz.play(2, 2));
     }
 
     @Test
     void testWithNumberIs3() {
-        assertEquals("Fizz", Fizzbuzz.play(3));
+        assertEquals("Fizz", Fizzbuzz.play(3, 3));
     }
 
     @Test
     void testWithNumberIs6() {
-        assertEquals("Fizz", Fizzbuzz.play(6));
+        assertEquals("Fizz", Fizzbuzz.play(6, 6));
     }
 
     @Test
     void testWithNumberIs9() {
-        assertEquals("Fizz", Fizzbuzz.play(9));
+        assertEquals("Fizz", Fizzbuzz.play(9, 9));
     }
 
     @Test
     void testWithNumberIs5() {
-        assertEquals("Buzz", Fizzbuzz.play(5));
+        assertEquals("Buzz", Fizzbuzz.play(5, 5));
     }
 
     @Test
     void testWithNumberIs10() {
-        assertEquals("Buzz", Fizzbuzz.play(10));
+        assertEquals("Buzz", Fizzbuzz.play(10, 10));
     }
 
     @Test
     void testWithNumberIs15() {
-        assertEquals("FizzBuzz", Fizzbuzz.play(15));
+        assertEquals("FizzBuzz", Fizzbuzz.play(15, 15));
     }
 
     @Test
     void testWithNumberIs30() {
-        assertEquals("FizzBuzz", Fizzbuzz.play(30));
+        assertEquals("FizzBuzz", Fizzbuzz.play(30, 30));
+    }
+
+    @Test
+    void testWith2Numbers() {
+        assertEquals("12", Fizzbuzz.play(1, 2));
     }
 
 }
